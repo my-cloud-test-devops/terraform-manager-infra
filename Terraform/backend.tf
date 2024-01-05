097b73680db4ddd126f18f91b0f47ea8ba58ae98
cornellues <cornellues@hotmail.com>
2024-01-05 13:27:16 +0530

    modified-providers

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
   backend "s3" {
     bucket = "github-actions-demo-2024"
     key    = "github-actions-new.tfstate"
