25d469011cc818be0a96e5aaf45cf5b9cd1801a1
cornellues <cornellues@hotmail.com>
2024-01-06 14:07:43 +0530

    s3-readded

--- backend.tf
+++ backend.tf
@@ -1,27 +1,7 @@
- # CREATE S3 BUCKET
- 
- resource "aws_s3_bucket" "my_bucket" {
-  bucket = "github-new-bucket-test-2024"
-  versioning {
-   enabled = true
-  }
-  server_side_encryption_configuration{
-   rule {
-     apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
-    }
+ terraform {
+  backend "s3" {
+    bucket = "github-new-bucket-test-2024"
+    key    = "github-actions-new.tfstate"
+    region = "us-east-1"
   }
   }
- }
-
- #Crearte DynamoDB
- resource "aws_dynamodb_table" "statelock" {
-  name = "state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
-   
-  attribute {
-     name = "LockID"
-     type = "S"
-   }
- }
