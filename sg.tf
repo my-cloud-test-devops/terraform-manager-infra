01d7e1584611d9f5c25c961c6471c004d43ad1e7
cornellues <cornellues@hotmail.com>
2024-01-07 15:36:32 +0530

    lb-vpc-tagged

--- sg.tf
+++ sg.tf
@@ -1,6 +1,6 @@
 #Load Balancer
 resource "aws_security_group" "demo_lb_sg" {
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.demo_vpc
   name        = var.security_group_name
   description = var.security_group_description
 
