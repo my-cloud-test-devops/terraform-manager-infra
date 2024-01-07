03ea4006ff1dc62eb26fae08733570bc78d1dd01
cornellues <cornellues@hotmail.com>
2024-01-07 14:08:04 +0530

    modifed-subnet

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ default = {
 # Public PUB_CIDR
 variable "pub_cidr" {
     type = list(string)
-    default = [ "10.0.1.0/24", "10.0.2.0/24" ]
+    default = [ "10.0.1.0/25, 10.0.2.0/25" ]
   
 }
 
