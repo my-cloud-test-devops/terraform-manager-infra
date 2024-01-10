abe89a4820bab6f386c1059cbd1ae1bfc52a6374
cornellues <cornellues@hotmail.com>
2024-01-10 13:42:10 +0530

    variables-name-commented

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 # variables.tf
 
-variable "aws_access_key" {
-  description = "AWS Access Key ID"
-}
+# variable "aws_access_key" {
+#   description = "AWS Access Key ID"
+# }
 
-variable "aws_secret_key" {
-  description = "AWS Secret Access Key"
-}
+# variable "aws_secret_key" {
+#   description = "AWS Secret Access Key"
+# }
 
-variable "db_username" {
-  description = "Database username"
-  type        = string
-}
+# variable "db_username" {
+#   description = "Database username"
+#   type        = string
+# }
 
-variable "db_password" {
-  description = "Database password"
-  type        = string
-}
+# variable "db_password" {
+#   description = "Database password"
+#   type        = string
+# }
 
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-}
+# variable "bucket_name" {
+#   description = "Name of the S3 bucket"
+# }
